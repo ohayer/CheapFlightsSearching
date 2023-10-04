3c0b05de0edd2ab4f2394a634f5412577d65b84a
ohayer <olik03.2003@wp.pl>
2023-10-04 20:31:43 +0200

    Building project

---
+++ src/main/java/pl/ohayer/flyticket/FlyTicketApplication.java
@@ -0,0 +1,13 @@
+package pl.ohayer.flyticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlyTicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlyTicketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/ohayer/flyticket/FlyTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.ohayer.flyticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlyTicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
